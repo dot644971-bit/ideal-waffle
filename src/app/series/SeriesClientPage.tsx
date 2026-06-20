b87d2030934862a19ff5ce09a37e2248f4011442
dot644971-bit <dot644971@gmail.com>
2026-06-21 00:54:39 +0300

    Add files via upload

--- src/app/series/SeriesClientPage.tsx
+++ src/app/series/SeriesClientPage.tsx
@@ -1,20 +1,7 @@
 'use client';
 
-import { useSearchParams } from 'next/navigation';
-import { useEffect } from 'react';
-import { useAppStore } from '@/lib/store';
 import SeriesPage from '@/components/Series';
 
 export default function SeriesClientPage() {
-  const searchParams = useSearchParams();
-  const slug = searchParams.get('slug') || '';
-  const setPageParams = useAppStore((s) => s.setPageParams);
-
-  useEffect(() => {
-    if (slug) {
-      setPageParams({ slug });
-    }
-  }, [slug, setPageParams]);
-
   return <SeriesPage />;
 }
